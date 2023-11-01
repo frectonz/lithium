1839dc04ff432b8b8f936495dc1d563944058b76
frectonz <fraol0912@gmail.com>
2023-11-01 23:30:04 +0300

    feat: add ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
+use std::io::stdout;
+
 use anyhow::Result;
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    prelude::CrosstermBackend,
+    widgets::{Block, Borders, Paragraph},
+    Frame, Terminal,
+};
 
 fn main() -> Result<()> {
+    enable_raw_mode()?;
+    stdout().execute(EnterAlternateScreen)?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+
+    let mut should_quit = false;
+    while !should_quit {
+        terminal.draw(ui)?;
+        should_quit = handle_events()?;
+    }
+
+    disable_raw_mode()?;
+    stdout().execute(LeaveAlternateScreen)?;
+
     let manager = battery::Manager::new()?;
 
     for maybe_battery in manager.batteries()? {
@@ -9,4 +34,23 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn handle_events() -> Result<bool> {
+    if event::poll(std::time::Duration::from_millis(50))? {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(true);
+            }
+        }
+    }
+    Ok(false)
+}
+
+fn ui(frame: &mut Frame) {
+    frame.render_widget(
+        Paragraph::new("Hello World!")
+            .block(Block::default().title("Greeting").borders(Borders::ALL)),
+        frame.size(),
+    );
 }
