b853710ec8d42ff1785803bdc070bafe65193bb2
frectonz <fraol0912@gmail.com>
2023-11-03 21:12:14 +0300

    feat: group the state into a struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::io::stdout;
+use std::io::{stdout, Stdout};
 
-use anyhow::Result;
-use battery::Battery;
+use anyhow::{Context, Result};
+use battery::{Battery, Manager};
 use crossterm::{
     event::{self, Event, KeyCode},
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
@@ -10,47 +10,75 @@ use crossterm::{
 use ratatui::{
     prelude::*,
     widgets::{Block, Borders, Paragraph},
-    Frame, Terminal,
+    Terminal,
 };
 
 fn main() -> Result<()> {
-    enable_raw_mode()?;
-    stdout().execute(EnterAlternateScreen)?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
-
-    let manager = battery::Manager::new()?;
-
-    let battery = manager.batteries()?.next().unwrap()?;
-
-    let mut should_quit = false;
-    while !should_quit {
-        terminal.draw(ui(&battery))?;
-        should_quit = handle_events()?;
-    }
-
-    disable_raw_mode()?;
-    stdout().execute(LeaveAlternateScreen)?;
-
+    let mut app = App::new()?;
+    app.run()?;
+    app.clear()?;
     Ok(())
 }
 
-fn handle_events() -> Result<bool> {
-    if event::poll(std::time::Duration::from_millis(50))? {
-        if let Event::Key(key) = event::read()? {
-            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(true);
-            }
-        }
-    }
-    Ok(false)
+struct App {
+    manager: Manager,
+    battery: Battery,
+    should_quit: bool,
+    terminal: Terminal<CrosstermBackend<Stdout>>,
 }
 
-fn ui(battery: &Battery) -> impl for<'a, 'b> Fn(&'a mut Frame<'b>) + '_ {
-    |frame: &mut Frame| {
-        let percent = battery.state_of_charge().value * 100.;
-        let percent = percent.to_string();
-        let rect = centered_rect(frame.size(), percent.len() as u16, 1);
-        frame.render_widget(Paragraph::new(percent), rect);
+impl App {
+    fn new() -> Result<Self> {
+        enable_raw_mode()?;
+        stdout().execute(EnterAlternateScreen)?;
+
+        let terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+
+        let manager = battery::Manager::new()?;
+        let battery = manager.batteries()?.next().context("no battery found")??;
+
+        Ok(Self {
+            terminal,
+            battery,
+            manager,
+            should_quit: false,
+        })
+    }
+
+    fn clear(&self) -> Result<()> {
+        disable_raw_mode()?;
+        stdout().execute(LeaveAlternateScreen)?;
+        Ok(())
+    }
+
+    fn handle_events(&mut self) -> Result<()> {
+        if event::poll(std::time::Duration::from_millis(50))? {
+            if let Event::Key(key) = event::read()? {
+                if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    self.should_quit = true;
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn run(&mut self) -> Result<()> {
+        while !self.should_quit {
+            self.terminal.draw(|frame| {
+                let percent = self.battery.state_of_charge().value * 100.;
+                let percent = percent.to_string();
+
+                let rect = centered_rect(frame.size(), percent.len() as u16, 1);
+                frame.render_widget(Paragraph::new(percent), rect);
+            })?;
+
+            self.handle_events()?;
+
+            // self.manager.refresh(&mut self.battery)?;
+        }
+
+        Ok(())
     }
 }
 
